c6b57b035621f1b5517fa894b9098ad4c98d7c68
anil <anil.pande82@gmail.com>
2024-06-17 14:15:00 +0200

    Clean up the response

--- container/llm/embedding/search_result.py
+++ container/llm/embedding/search_result.py
@@ -2,44 +2,20 @@ import os
 
 from dotenv import load_dotenv
 from langchain.chains.llm import LLMChain
-from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain_core.prompts import PromptTemplate
+from langchain_huggingface import HuggingFaceEndpoint
 from qdrant_client import QdrantClient
 
 load_dotenv(dotenv_path="../../../dev.env")
 
-client = QdrantClient(
-    url=os.getenv("QDRANT_HOST"),
-    api_key=os.getenv("QDRANT_API_KEY"),
-    grpc_port=6334,
-    prefer_grpc=True
-)
+repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
 
-# retrieve = cli
-# embeddings = FastEmbedEmbeddings()
-# qdrant = Qdrant(client, os.getenv('QDRANT_COLLECTION'), vector_name="fast-bge-small-en")
-# retriever = qdrant.as_retriever(search_type="mmr",
-#                                 search_kwargs={'k': 6, 'lambda_mult': 0.25})
-# search_result = retriever.invoke("List of yoga asana for lung related issue")
-# search_result = qdrant.similarity_search("List of yoga asana for lung related issue")
-
-search_result = client.query(
-    collection_name=os.getenv('QDRANT_COLLECTION'),
-    query_text="List of yoga asana for lung related issue",
-    limit=10
-)
-
-client.close()
-
-print(search_result)
-
-question = "What is Experimentation?"
 prompt_template = """SYSTEM: You are an intelligent Yoga assistant helping the users with their questions on Yoga.
 
 Question: {question}
 
 Strictly Use ONLY the following pieces of context to answer the question at the end. Think step-by-step and then answer.
-
+Context contain reference and metadata as well. Use that improve your answer quality.
 Do not try to make up an answer: - If the answer to the question cannot be determined from the context alone, 
 say "I cannot determine the answer to that." - If the context is empty, just say "I do not know the answer to that."
 
@@ -54,17 +30,36 @@ prompt = PromptTemplate(
     template=prompt_template, input_variables=["context", "question"]
 )
 
-repo_id = "meta-llama/Meta-Llama-3-8B"
 
-llm = HuggingFaceEndpoint(
-    repo_id=repo_id,
-    temperature=0.5,
-    huggingfacehub_api_token=os.getenv('HF_API_KEY'),
-)
+def get_similar_content(input_query: str = None):
+    client = QdrantClient(
+        url=os.getenv("QDRANT_HOST"),
+        api_key=os.getenv("QDRANT_API_KEY"),
+        grpc_port=6334,
+        prefer_grpc=True
+    )
+    search_result = client.query(
+        collection_name=os.getenv('QDRANT_COLLECTION'),
+        query_text=input_query,
+        limit=10
+    )
+    client.close()
+    return search_result
 
-chain = LLMChain(llm=llm, prompt=prompt)
-summary = chain.run(search_result, "List of yoga asana for lung related issue")
 
-# Print the summary
-print("Summary:")
-print(summary)
+def generate_answer(query: str = None):
+    llm = HuggingFaceEndpoint(
+        repo_id=repo_id,
+        max_length=512,
+        temperature=0.25,
+        huggingfacehub_api_token=os.getenv('HUGGINGFACEHUB_API_TOKEN'),
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
+    answer = chain({"context": get_similar_content(query), "question": query})
+    # Print the summary
+    print("Question:", answer['question'])
+    print("Answer:", answer['text'])
+
+
+generate_answer(query="Explain Yogic Counselling?")
+generate_answer(query="List me 5 asanas which is beneficial for lung related issues?")
